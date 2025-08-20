9ccb38f82f6c0da8fa7f004c4525b2f8cf9264b9
Marcus Fan <marcus.fan@veloceapps.com>
2025-08-20 12:40:35 -0700

    Adjusting token upload page

--- app/upload/[token]/page.tsx
+++ app/upload/[token]/page.tsx
@@ -10,7 +10,7 @@ export default async function UploadPage({ params }: PageProps) {
   const { token } = await params;
 
   return (
-    <div className="bg-gradient-to-b from-background to-background/50 min-h-screen">
+    <div className="bg-gradient-to-b from-background to-background/50 min-h-screen flex flex-col items-center justify-center">
       <div className="flex flex-col items-center justify-center h-full p-8">
         <h1 className="text-4xl font-[Rubik] mb-8">Upload Video</h1>
         <ClientUpload token={token} />
